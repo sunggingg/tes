8ea8e157287843125730e7f3d6a81d2bf108967b
sunggingg <46112680+sunggingg@users.noreply.github.com>
2020-08-10 21:29:49 +0700

    Create setting.php

---
+++ setting.php
@@ -0,0 +1,26 @@
+<?php
+
+$br="\033[34m";$tr="\033[36m";$ijo="\033[92m";
+$pth="\033[37m";$pnk="\033[35m";$red="\033[31m";
+$kn="\033[33m";$blk="\033[8m";$nt="\033[0"."m";
+
+///
+
+
+$name = "\r{$red}
+{$tr}[-] server {$ijo}ganteng
+{$tr}[-] forum diskusi : {$ijo}https://t.me/tuyulgaple
+{$br}[caping] {$tr} creator {$br} sungging {$tr} {$nt}versi {$ijo}1.0
+{$tr}[info] {$pth}free version !!!
+{$pth}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{$br}- support by    {$ijo}-> youtube adibordir
+{$br}- support by    {$ijo}-> youtube sungging
+{$pth}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{$red}[!] warning !!!
+{$kn}[-] enjoy your life
+{$kn}[-] do with your own risk
+
+{$pth}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+
+
+<?
